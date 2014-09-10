089c15949ec771a4699d658cc29fea8e319b193a
ShevT <shev_t@mail.ru>
2014-09-10 06:57:46 +0400

    build_front: ccache dir

--- build_front.sh
+++ build_front.sh
@@ -15,7 +15,7 @@ export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/AndroidSources/linaro-4.9.2/bin/arm-eabi-
 export LOCALVERSION="-stable-f2fs"
-export CCACHE_DIR=~/.ccache/kernel
+export CCACHE_DIR=~/.ccache/kernel31
 ccache -M 5G
 ##############################################################################
 # set variables
