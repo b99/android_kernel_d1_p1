368ac69a9c08c839e349a08e1bc70405e2a605f4
ShevT <shev_t@mail.ru>
2014-09-10 06:36:22 +0400

    build_front: update

--- build_front.sh
+++ build_front.sh
@@ -13,9 +13,10 @@
 ##############################################################################
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=~/AndroidSources/linaro-4.9.1/bin/arm-eabi-
+export CROSS_COMPILE=~/AndroidSources/linaro-4.9.2/bin/arm-eabi-
 export LOCALVERSION="-stable-f2fs"
 export CCACHE_DIR=~/.ccache/kernel
+ccache -M 5G
 ##############################################################################
 # set variables
 ##############################################################################
@@ -27,7 +28,7 @@ STRIP=${CROSS_COMPILE}strip
 ##############################################################################
 mkdir -p $KERNEL_OUT
 make O=$KERNEL_OUT front_defconfig
-make -j6 O=$KERNEL_OUT
+make -j10 O=$KERNEL_OUT
 
 if [ -f $KERNEL_OUT/arch/arm/boot/zImage ]
 then
@@ -41,7 +42,7 @@ fi
 if [ -f $KERNEL_OUT/arch/arm/boot/zImage ]
 then
     make clean -C $KERNELDIR/sgx/pvr-source/eurasiacon/build/linux2/omap4430_android
-    make -j6 -C $KERNELDIR/sgx/pvr-source/eurasiacon/build/linux2/omap4430_android KERNELDIR=$KERNEL_OUT TARGET_PRODUCT="blaze" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.0
+    make -j10 -C $KERNELDIR/sgx/pvr-source/eurasiacon/build/linux2/omap4430_android KERNELDIR=$KERNEL_OUT TARGET_PRODUCT="blaze" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.0
     mv $KERNELDIR/sgx/pvr-source/eurasiacon/binary2_540_120_omap4430_android_release/target/kbuild/pvrsrvkm_sgx540_120.ko $KERNEL_OUT
     $STRIP --strip-unneeded $KERNEL_OUT/pvrsrvkm_sgx540_120.ko
     make clean -C $KERNELDIR/sgx/pvr-source/eurasiacon/build/linux2/omap4430_android
