5ab6d22033770d73aad75ef25ce2a265cbf579ea
ShevT <shev_t@mail.ru>
2015-03-12 13:00:49 +0300

    Revert "Staging: zram: Fix memory leak by refcount mismatch"
    This reverts commit bf57a253f14490ba3eee49e7634e14833350ee56.

--- drivers/staging/zram/zram_sysfs.c
+++ drivers/staging/zram/zram_sysfs.c
@@ -95,32 +95,23 @@ static ssize_t reset_store(struct device *dev,
 		return -ENOMEM;
 
 	/* Do not reset an active device! */
-	if (bdev->bd_holders) {
-		ret = -EBUSY;
-		goto out;
-	}
+	if (bdev->bd_holders)
+		return -EBUSY;
 
 	ret = strict_strtoul(buf, 10, &do_reset);
 	if (ret)
-		goto out;
+		return ret;
 
-	if (!do_reset) {
-		ret = -EINVAL;
-		goto out;
-	}
+	if (!do_reset)
+		return -EINVAL;
 
 	/* Make sure all pending I/O is finished */
 	fsync_bdev(bdev);
-	bdput(bdev);
 
 	if (zram->init_done)
 		zram_reset_device(zram);
 
 	return len;
-
-out:
-	bdput(bdev);
-	return ret;
 }
 
 static ssize_t num_reads_show(struct device *dev,
