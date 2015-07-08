511f3146eb46ec6383899b2ea040ae0b4c6ae094
ShevT <shev_t@mail.ru>
2015-07-08 11:26:21 +0300

    build_kernel: version kernel 31+

--- build_kernel.sh
+++ build_kernel.sh
@@ -15,7 +15,7 @@
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/AndroidSources/linaro-4.9.3-a9/bin/arm-eabi-
-export LOCALVERSION="-31_stable"
+export LOCALVERSION="-31+"
 export CCACHE_DIR=~/.ccache/kernel31
 export MY_CONFIG=front_defconfig
 ccache -M 5G
