4f0b23fd69c96d2f96e7542dac9b74665534cc95
ShevT <shev_t@mail.ru>
2015-07-24 06:15:35 +0300

    build_kernel.sh: set cm12x version

--- build_kernel.sh
+++ build_kernel.sh
@@ -15,7 +15,7 @@
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/AndroidSources/linaro-4.9.4-a9/bin/arm-eabi-
-export LOCALVERSION="-next"
+export LOCALVERSION="-cm12x"
 export CCACHE_DIR=~/.ccache/front-kernel
 export MY_CONFIG=front_defconfig
 ccache -M 5G
